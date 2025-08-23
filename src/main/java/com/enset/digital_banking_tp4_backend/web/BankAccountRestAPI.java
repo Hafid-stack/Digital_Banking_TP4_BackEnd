e81691b4725bb08b05eab466d21d32304acc98c6
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-08-23 21:58:40 +0100

    Adding swager ui

---
+++ src/main/java/com/enset/digital_banking_tp4_backend/web/BankAccountRestAPI.java
@@ -0,0 +1,11 @@
+package com.enset.digital_banking_tp4_backend.web;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+public class BankAccountRestAPI {
+}
